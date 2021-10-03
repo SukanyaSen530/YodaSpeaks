a1b098d2a2a3a5fc736cc50f4d2a81864b4ab2e9
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2021-10-03 11:50:31 +0530

    fix- error handling code

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-const form = document.querySelector('#form');
-const output = document.querySelector('#output');
+const form = document.querySelector("#form");
+const output = document.querySelector("#output");
 let url = "https://api.funtranslations.com/translate/yoda.json?";
 
-
 function errorHandler(error) {
-    alert("Sorry! Error :(, Try again later!");
-    console.log(error);
+  alert("Sorry! Error :(, Try again later!");
+  console.log(error);
 }
 
-form.addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    let dataurl = `${url}text=${form.elements.word.value}`;
-    fetch(dataurl)
-        .then((res) => {
-            res.json().then((data) => {
-                console.log(data);
-                console.log(data.contents.translated);
-                output.innerText = `${data.contents.translated}`;
-        }).catch(error => {
-            console.log(errorHandler);
-        })
-    })
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  let dataurl = `${url}text=${form.elements.word.value}`;
+  fetch(dataurl).then((res) => {
+    res
+      .json()
+      .then((data) => {
+        console.log(data);
+        console.log(data.contents.translated);
+        output.innerText = `${data.contents.translated}`;
+      })
+      .catch((error) => {
+        errorHandler(error);
+      });
+  });
 });
-
-
-
-
